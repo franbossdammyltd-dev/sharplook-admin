6671836bd765dc067f97573579bf1efea3e6cf9c
zumerhu <zumerhub@gmail.com>
2025-10-15 17:55:57 +0100

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+// import { viteStaticCopy } from "vite-plugin-static-copy";
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//     viteStaticCopy({
+//       targets: [
+//         {
+//           src: "public/_redirects", 
+//           dest: ".",                
+//         },
+//       ],
+//     }),
+//   ],
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
   plugins: [
@@ -10,10 +33,19 @@ export default defineConfig({
     viteStaticCopy({
       targets: [
         {
-          src: "public/_redirects", 
-          dest: ".",                
+          src: "public/_redirects",
+          dest: ".",
         },
       ],
     }),
   ],
+
+  preview: {
+    host: "0.0.0.0",
+    port: Number(process.env.PORT) || 10000, // ✅ safe and clean
+    allowedHosts: [
+      "sharplook-admin-1zea.onrender.com",
+      "localhost",
+    ],
+  },
 });
